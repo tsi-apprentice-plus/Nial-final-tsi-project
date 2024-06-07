a3231cfadce36a2a02ed9c24b5642cd57d6e99c2
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-06-07 15:33:16 +0100

    create posts in icds ui

--- icds-frontend/src/app/components/CreatePost.tsx
+++ icds-frontend/src/app/components/CreatePost.tsx
@@ -1,11 +1,22 @@
+"use client"
+import { IcTextField, IcButton } from "@ukic/react";
+import { createPost } from "@/utils/route";
+
+
 export default function CreatePost() {
+  function handleSubmit() {
+    const title = document.getElementById("create-title") as HTMLInputElement;
+    console.log(title.value);
+    createPost({ content: title.value });
+  }
   return (
-    <div>
-      <form>
-        <label htmlFor="content">Content</label>
-        <input type="text" id="content" name="content" />
-        <button type="submit">Submit</button>
-      </form>
+    <div className="container mx-auto px-4">
+      <div className="flex justify-center">
+        <form>
+        <IcTextField rows={3} label="Create Post" placeholder="Please enter…" id="create-title"/>
+        <IcButton onClick={handleSubmit}>Submit</IcButton>
+        </form>
+      </div>
     </div>
   );
 }
