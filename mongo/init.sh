357cfd05c17abd01f533593dabb9dbd8993f1af2
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-06-11 18:47:09 +0100

    fix??

--- mongo/init.sh
+++ mongo/init.sh
@@ -4,5 +4,5 @@
 sleep 10
 
 # Import data into the 'socialAPI' database
-mongoimport --host mongo --db socialAPI --collection posts --file /docker-entrypoint-initdb.d/socialAPI.posts.json --jsonArray
-mongoimport --host mongo --db socialAPI --collection users --file /docker-entrypoint-initdb.d/socialAPI.users.json --jsonArray
+mongoimport --host db --db socialAPI --collection posts --file /docker-entrypoint-initdb.d/socialAPI.posts.json --jsonArray
+mongoimport --host db --db socialAPI --collection users --file /docker-entrypoint-initdb.d/socialAPI.users.json --jsonArray
