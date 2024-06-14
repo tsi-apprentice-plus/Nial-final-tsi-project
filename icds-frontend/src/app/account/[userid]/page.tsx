a61dfcce284c37d8d03a9f9b655852cdd724b047
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-06-14 16:56:49 +0100

    add dynamic route

---
+++ icds-frontend/src/app/account/[userid]/page.tsx
@@ -0,0 +1,27 @@
+import MenuAppBar from "@/app/components/search";
+import { getPostsUser, getUser } from "@/utils/route";
+import { Posts } from "@/types/post";
+import PostsList from "@/app/components/PostsList";
+import { Suspense } from "react";
+import UserHeader from "@/app/components/UserHeader";
+import { User } from "@/types/user";
+
+export default async function AccountPage({ params }: Readonly<{ params: { userid: number } }>) {
+  const { userid } = params;
+  const usersPosts: Posts = await getPostsUser(userid);
+  const users = await getUser(userid);
+  const user: User = users[0];
+  console.log(usersPosts);
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <MenuAppBar />
+        <br />
+        <UserHeader user={user} />
+        <br />
+        <PostsList posts={usersPosts} showDeletePost={true} />
+      </Suspense>
+    </div>
+  );
+}
+export const dynamic = "force-dynamic";
---
+++ icds-frontend/src/app/components/UserHeader.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { IcTypography } from "@ukic/react";
+import { User } from "@/types/user";
+
+interface Props {
+  user: Readonly<User>;
+}
+
+export default function UserHeader({ user }: Readonly<Props>) {
+
+  return (
+    <div className="flex justify-center">
+      <div className="bg-white p-4 rounded-lg shadow-md w-fit">
+        <IcTypography variant="h4">@{user.username}</IcTypography>
+        <IcTypography variant="caption">{user.email}</IcTypography>
+        <IcTypography variant="caption">{user.id}</IcTypography>
+      </div>
+    </div>
+  );
+}
