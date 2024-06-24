e36f2f93df9037941b8852dfafc72c54ceb08c0c
Nial <48334675+nmcc1212@users.noreply.github.com>
2024-06-25 00:32:20 +0100

    chore: Update docker-compose command in GitHub Actions workflow

---
+++ kube/variables.tf
@@ -0,0 +1,14 @@
+variable "my_name" {
+  type    = string
+  default = "nial"
+}
+
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "domain" {
+  type    = string
+  default = "netbuildertraining.com"
+}
